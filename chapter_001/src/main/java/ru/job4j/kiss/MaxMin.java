7803195aeea5b83f93bc717ef686b8a5615d26f4
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-03-23 02:24:06 +0900

    1. Принципы Kiss, Dry и Yagni[#256519]

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -10,23 +10,41 @@ import java.util.List;
 public class MaxMin {
     /**
      * find max element
-     * @param value - List values
+     *
+     * @param value      - List values
      * @param comparator - Comparator implements
-     * @param <T> - type list elements
+     * @param <T>        - type list elements
      * @return - max element from list
      */
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return Collections.max(value, comparator);
+        return search(value, comparator);
     }
 
     /**
      * find min element
-     * @param value - List values
+     *
+     * @param value      - List values
      * @param comparator - Comparator implements
-     * @param <T> - type list elements
+     * @param <T>        - type list elements
      * @return - max element from list
      */
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return Collections.min(value, comparator);
+        return search(value, comparator);
+    }
+
+    /**
+     * travers list elements and search use comparator
+     *
+     * @param values     - list of values
+     * @param comparator - compare function
+     * @param <T>        - generics
+     * @return - result of search from compare
+     */
+    public <T> T search(List<T> values, Comparator<T> comparator) {
+        T res = values.get(0);
+        for (T value : values) {
+            res = comparator.compare(res, value) > 0 ? res : value;
+        }
+        return res;
     }
 }
