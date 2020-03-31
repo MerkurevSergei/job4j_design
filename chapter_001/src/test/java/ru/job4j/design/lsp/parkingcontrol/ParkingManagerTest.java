050b43c4076c35d9e26d2314f65b343b619b3c78
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-03-31 17:21:13 +0900

    2. Парковка машин[#256535] - улучшил читаемость тестов

--- chapter_001/src/test/java/ru/job4j/design/lsp/parkingcontrol/ParkingManagerTest.java
+++ chapter_001/src/test/java/ru/job4j/design/lsp/parkingcontrol/ParkingManagerTest.java
@@ -11,68 +11,58 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ParkingManagerTest {
-    List<Parking> parkings = new ArrayList<>();
-    List<Vehicle> vehicles = new ArrayList<>();
-    @Before
-    public void setUp() {
-        parkings.add(new ParkingTrack(10));
-        parkings.add(new ParkingCar(10));
-
-        vehicles.add(new Track(6));
-        vehicles.add(new Car());
-        vehicles.add(new Car());
-        vehicles.add(new Car());
-        vehicles.add(new Track(5));
-        vehicles.add(new Car());
-        vehicles.add(new Car());
-
-    }
 
     @Test
     public void executeSuccess() throws Exception {
-        ParkingTrack expectedParkingTrack = new ParkingTrack(10);
-        expectedParkingTrack.add(vehicles.get(0));
-        ParkingCar expectedParkingCar = new ParkingCar(10);
-        expectedParkingCar.add(vehicles.get(1));
-        expectedParkingCar.add(vehicles.get(2));
-        expectedParkingCar.add(vehicles.get(3));
-        expectedParkingCar.add(vehicles.get(4));
-        expectedParkingCar.add(vehicles.get(5));
-        expectedParkingCar.add(vehicles.get(6));
+        // Vehicles for distribute
+        Car car = new Car();
+        Track track2 = new Track(2);
+        Track track3 = new Track(3);
+
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(car);
+        vehicles.add(track2);
+        vehicles.add(track3);
+
+        // Setting the expected parkings state
+        ParkingTrack expParkingTrack = new ParkingTrack(3);
+        expParkingTrack.add(track2);
+
+        ParkingCar expParkingCar = new ParkingCar(5);
+        expParkingCar.add(car);
+        expParkingCar.add(track3);
 
         List<Parking> expectedParkings = new ArrayList<>();
-        expectedParkings.add(expectedParkingTrack);
-        expectedParkings.add(expectedParkingCar);
+        expectedParkings.add(expParkingTrack);
+        expectedParkings.add(expParkingCar);
 
-        ParkingManager manager = new ParkingManager(parkings);
+        // Out result after distribute
+        List<Parking> outParkings = new ArrayList<>();
+        outParkings.add(new ParkingTrack(3));
+        outParkings.add(new ParkingCar(5));
+        ParkingManager manager = new ParkingManager(outParkings);
         manager.execute(vehicles);
 
-        assertThat(expectedParkings, is(parkings));
+        assertThat(expectedParkings, is(outParkings));
     }
 
     @Test(expected = Exception.class)
     public void executeFailed() throws Exception {
-        vehicles.add(new Car());
+        // Vehicles for distribute
+        List<Vehicle> vehicles = new ArrayList<>();
+        Track track2 = new Track(2);
+        vehicles.add(track2);
 
-        ParkingTrack expectedParkingTrack = new ParkingTrack(10);
-        expectedParkingTrack.add(vehicles.get(0));
-        ParkingCar expectedParkingCar = new ParkingCar(10);
-        expectedParkingCar.add(vehicles.get(1));
-        expectedParkingCar.add(vehicles.get(2));
-        expectedParkingCar.add(vehicles.get(3));
-        expectedParkingCar.add(vehicles.get(4));
-        expectedParkingCar.add(vehicles.get(5));
-        expectedParkingCar.add(vehicles.get(6));
-
+        // Setting the expected parkings state
         List<Parking> expectedParkings = new ArrayList<>();
-        expectedParkings.add(expectedParkingTrack);
-        expectedParkings.add(expectedParkingCar);
 
-        ParkingManager manager = new ParkingManager(parkings);
+        // Out result after distribute
+        List<Parking> outParkings = new ArrayList<>();
+        outParkings.add(new ParkingTrack(1));
+        ParkingManager manager = new ParkingManager(outParkings);
         manager.execute(vehicles);
 
-        assertThat(expectedParkings, is(parkings));
+        assertThat(expectedParkings, is(outParkings));
     }
-
 
 }
