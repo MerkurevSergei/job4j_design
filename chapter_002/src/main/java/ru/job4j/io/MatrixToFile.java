35b7be81c06afd8429920be95cdbce4c14941374
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-04-05 12:40:59 +0900

    0.1. Система ввода и вывода.[#256501]

---
+++ chapter_002/src/main/java/ru/job4j/io/MatrixToFile.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.StringJoiner;
+
+/**
+ * Example output data to file.
+ */
+public class MatrixToFile {
+    String matrixString;
+
+    /**
+     * Initialization matrix string.
+     */
+    public MatrixToFile() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append((i + 1) * (j + 1)).append(" ");
+            }
+            sb.append(System.lineSeparator());
+        }
+        matrixString = sb.toString();
+    }
+
+    /**
+     * Write String matrixString to file.
+     */
+    public void write() {
+        try (FileOutputStream out = new FileOutputStream("C:\\temp\\ExampleMatrix.txt")) {
+            out.write(matrixString.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/MatrixToFileTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MatrixToFileTest {
+
+    @Test
+    public void write() {
+        String expected = "1 2 3 4 5 6 7 8 9 " + System.lineSeparator()
+                + "2 4 6 8 10 12 14 16 18 " + System.lineSeparator()
+                + "3 6 9 12 15 18 21 24 27 " + System.lineSeparator()
+                + "4 8 12 16 20 24 28 32 36 " + System.lineSeparator()
+                + "5 10 15 20 25 30 35 40 45 " + System.lineSeparator()
+                + "6 12 18 24 30 36 42 48 54 " + System.lineSeparator()
+                + "7 14 21 28 35 42 49 56 63 " + System.lineSeparator()
+                + "8 16 24 32 40 48 56 64 72 " + System.lineSeparator()
+                + "9 18 27 36 45 54 63 72 81 " + System.lineSeparator();
+
+        MatrixToFile matrixToFile = new MatrixToFile();
+        matrixToFile.write();
+        StringBuilder out = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\temp\\ExampleMatrix.txt"))) {
+            while (reader.ready()) {
+                out.append(reader.readLine()).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(expected, is(out.toString()));
+    }
+}
