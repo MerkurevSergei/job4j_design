12408bc808263e90772d3720338b641ad84b09c8
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-04-07 16:46:02 +0900

    3.0. Тестирование IO[#256499]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.util.StringJoiner;
@@ -9,14 +11,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void unavailable() throws IOException {
         String expected = "10:57:01;10:59:01;" + System.lineSeparator() + "11:01:02;11:02:02;";
+
+        final String target = folder.newFile("target.txt").getAbsolutePath();
         Analizy analizy = new Analizy();
-        analizy.unavailable("src/test/resources/server.log", "src/test/resources/target.log");
+        analizy.unavailable("src/test/resources/server.txt", target);
         StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (final BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/target.log"))) {
+        try (final BufferedReader reader = new BufferedReader(new FileReader(target))) {
             reader.lines().forEach(out::add);
         }
         assertThat(expected, is(out.toString()));
