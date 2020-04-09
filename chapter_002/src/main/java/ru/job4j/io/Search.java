f357f4e30cf175ae55933cdc578245650cd07de8
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-04-09 12:55:51 +0900

    5. Валидация параметров запуска.[#256500]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -18,7 +18,9 @@ public class Search {
      */
     public static void main(String[] args) throws IOException {
         Path start = Paths.get("./chapter_002/src/main/java");
-        final List<String> files = search(start, "java");
+        List<String> files = search(start, "java");
+        System.out.println(files);
+        files = search(args);
         System.out.println(files);
     }
 
@@ -31,6 +33,20 @@ public class Search {
     public static List<String> search(Path root, String ext) throws IOException {
         final ListFileVisitor listFileVisitor = new ListFileVisitor(new ArrayList<>(), ext);
         Files.walkFileTree(root, listFileVisitor);
+        return listFileVisitor.getList();
+    }
+
+    /**
+     * @param args - command line arguments
+     * @return - result of search from extension
+     * @throws IOException - exception
+     */
+    public static List<String> search(String[] args) throws IOException {
+        if (args.length < 2 || !Paths.get(args[0]).toFile().exists() || Paths.get(args[0]).toFile().isFile()) {
+            throw new IllegalArgumentException("Command line arguments is not valid");
+        }
+        final ListFileVisitor listFileVisitor = new ListFileVisitor(new ArrayList<>(), args[1]);
+        Files.walkFileTree(Paths.get(args[0]), listFileVisitor);
         return listFileVisitor.getList();
     }
 }
--- chapter_002/src/test/java/ru/job4j/io/SearchTest.java
+++ chapter_002/src/test/java/ru/job4j/io/SearchTest.java
@@ -20,15 +20,43 @@ public class SearchTest {
 
     @Test
     public void search() throws IOException {
-        folder.newFile("one.txt");
         final File file = folder.newFile("two.java");
+        final File file1 = folder.newFile("five.java");
+        folder.newFile("one.txt");
         folder.newFile("three.pjava");
         folder.newFile("four.javac");
-        final File file1 = folder.newFile("five.java");
+
         String expected = String.format("[%s, %s]", file1.getAbsolutePath(), file.getAbsolutePath());
 
         final List<String> outList = Search.search(folder.getRoot().toPath(), "java");
         outList.sort(String::compareTo);
         assertThat(expected, is(outList.toString()));
+    }
+
+    @Test
+    public void whenSearchArgsSuccess() throws IOException {
+        final File file = folder.newFile("two.java");
+        final File file1 = folder.newFile("five.java");
+        folder.newFile("one.txt");
+        folder.newFile("three.pjava");
+        folder.newFile("four.javac");
+
+        String expected = String.format("[%s, %s]", file1.getAbsolutePath(), file.getAbsolutePath());
+
+        String[] args = new String[]{folder.getRoot().toPath().toString(), "java"};
+        final List<String> outList = Search.search(args);
+        outList.sort(String::compareTo);
+        assertThat(expected, is(outList.toString()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenSearchArgsIllegalArgumentException() throws IOException {
+        final File file = folder.newFile("two.java");
+        folder.newFile("one.txt");
+
+        String[] args = new String[]{file.toString(), "java"};
+        final List<String> outList = Search.search(args);
+        outList.sort(String::compareTo);
+        assertThat("", is(outList.toString()));
     }
 }
