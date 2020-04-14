3beefdb0d27125598e511b4617cb7a554f5c4473
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-04-14 20:17:42 +0900

    5.3.0 Создать метод delete для односвязного списка[#256462]

---
+++ chapter_003/src/main/java/ru/job4j/list/ForwardLinked.java
@@ -0,0 +1,100 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Forward Linked store
+ * @author Merkurev Sergei (merkurevsergei@yandex.ru)
+ * @version 0.1
+ * @since 0.1
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    /**
+     * top item
+     */
+    private Node<T> head;
+
+    /**
+     * @param value - value to be stored
+     */
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    /**
+     * deleted first element
+     */
+    public void deleteFirst() throws NoSuchElementException {
+        if (head == null) {
+            throw new NoSuchElementException("No such");
+        }
+        head = head.next;
+    }
+
+    /**
+     * @return iterator
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            /**
+             * set iterable node to head
+             */
+            Node<T> node = head;
+
+            /**
+             * @return true if has next
+             */
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            /**
+             * @return next value
+             */
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    /**
+     * @param <T> node type
+     */
+    private static class Node<T> {
+        /**
+         * node value
+         */
+        T value;
+        /**
+         * next node
+         */
+        Node<T> next;
+
+        /**
+         * @param value - init value
+         * @param next - next node
+         */
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
