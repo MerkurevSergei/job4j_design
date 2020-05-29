b493d0dd8c96b3af74e7b497fa6ce250940caf28
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-05-29 23:04:28 +0900

    Хранилище машин [#256454]. Исправление замечаний №1

--- chapter_004/src/main/java/ru/job4j/carstorage/create.sql
+++ chapter_004/src/main/java/ru/job4j/carstorage/create.sql
@@ -37,13 +37,14 @@ CREATE TABLE Transmission (
 	CONSTRAINT transmission_pkey PRIMARY KEY (id)
 );
 
+
 -- CAR TABLE
 CREATE TABLE Car (
-	id SERIAL NOT NULL, 
-	model varchar(200),
-	carcass_id integer REFERENCES Carcass (id),
-	engine_id integer REFERENCES Engine (id),
-	transmission_id integer REFERENCES Transmission (id),
+	id SERIAL NOT NULL,
+	model varchar(200) NOT NULL,
+	carcass_id integer REFERENCES Carcass (id) NOT NULL,
+	engine_id integer REFERENCES Engine (id) NOT NULL,
+	transmission_id integer REFERENCES Transmission (id) NOT NULL,
 	CONSTRAINT car_pkey PRIMARY KEY (id)
 );
 
@@ -59,7 +60,19 @@ INSERT INTO Carcass (id, style) VALUES
     (1, 'Minivan'),
 	(2, 'Sedan'),
     (3, 'Coupe'),
-    (4, 'Hardtop ');
+    (4, 'Hardtop'),
+	(5, '1'),
+    (6, '11'),
+    (7, '111'),
+	(8, '1111'),
+    (9, '2'),
+    (10, '22'),
+	(11, '222'),
+    (12, '2222'),
+    (13, '22222'),
+	(14, '222222'),
+    (15, '3'),
+    (16, '33');
 	
 INSERT INTO Engine (id, type) VALUES
     (1, 'Two-Stroke'),
@@ -73,4 +86,52 @@ INSERT INTO Transmission (id, type) VALUES
 INSERT INTO Car (id, model, carcass_id, engine_id, transmission_id) VALUES
     (1, 'Honda Life', 1, 1, 1),
 	(2, 'Toyota Aygo', 2, 2, 2),
-    (3, 'Honda Civic', 3, 2, 1);
+    (3, 'Honda Civic', 3, 2, 1),
+    (4, 'Honda Life', 1, 1, 1),
+	(5, 'Toyota Aygo', 2, 2, 2),
+    (6, 'Honda Civic', 3, 2, 1),
+    (7, 'Honda Life', 1, 1, 1),
+	(8, 'Toyota Aygo', 2, 2, 2),
+    (9, 'Honda Civic', 3, 2, 1),
+    (10, 'Honda Life', 1, 1, 1),
+	(11, 'Toyota Aygo', 2, 2, 2),
+    (12, 'Honda Civic', 3, 2, 1),
+    (13, 'Honda Life', 1, 1, 1),
+	(14, 'Toyota Aygo', 2, 2, 2),
+    (15, 'Honda Civic', 3, 2, 1),
+    (16, 'Honda Life', 1, 1, 1),
+	(17, 'Toyota Aygo', 2, 2, 2),
+    (18, 'Honda Civic', 3, 2, 1),
+    (19, 'Honda Life', 1, 1, 1),
+	(20, 'Toyota Aygo', 2, 2, 2),
+    (21, 'Honda Civic', 3, 2, 1),
+    (22, 'Honda Life', 1, 1, 1),
+	(23, 'Toyota Aygo', 2, 2, 2),
+    (24, 'Honda Civic', 3, 2, 1),
+    (25, 'Honda Life', 1, 1, 1),
+	(26, 'Toyota Aygo', 2, 2, 2),
+    (27, 'Honda Civic', 3, 2, 1),
+    (28, 'Honda Life', 1, 1, 1),
+	(29, 'Toyota Aygo', 2, 2, 2),
+    (30, 'Honda Civic', 3, 2, 1),
+    (31, 'Honda Civic', 3, 2, 1),
+    (32, 'Honda Life', 1, 1, 1),
+	(33, 'Toyota Aygo', 2, 2, 2),
+    (34, 'Honda Civic', 3, 2, 1),
+    (35, 'Honda Life', 1, 1, 1),
+	(36, 'Toyota Aygo', 2, 2, 2),
+    (37, 'Honda Civic', 3, 2, 1),
+    (38, 'Honda Life', 1, 1, 1),
+	(39, 'Toyota Aygo', 2, 2, 2),
+	(40, 'Toyota Aygo', 2, 2, 2),
+    (41, 'Honda Civic', 3, 2, 1),
+    (42, 'Honda Life', 1, 1, 1),
+	(43, 'Toyota Aygo', 2, 2, 2),
+    (44, 'Honda Civic', 3, 2, 1),
+    (45, 'Honda Life', 1, 1, 1),
+	(46, 'Toyota Aygo', 2, 2, 2),
+    (47, 'Honda Civic', 3, 2, 1),
+    (48, 'Honda Life', 1, 1, 1),
+	(49, 'Toyota Aygo', 2, 2, 2),
+    (50, 'Honda Civic', 3, 2, 1),
+	(51, 'Toyota Aygo', 2, 2, 2);
--- chapter_004/src/main/java/ru/job4j/carstorage/select.sql
+++ chapter_004/src/main/java/ru/job4j/carstorage/select.sql
@@ -7,34 +7,34 @@ select
     transmission.type
 from
     car c
-left outer join carcass on (c.carcass_id = carcass.id)
-left outer join engine on (c.engine_id = engine.id)
-left outer join transmission on (c.transmission_id = transmission.id);
+inner join carcass on (c.carcass_id = carcass.id)
+inner join engine on (c.engine_id = engine.id)
+inner join transmission on (c.transmission_id = transmission.id);
 
 -- 2. Вывести отдельно детали, которые не используются в машине, кузова, двигатели, коробки передач.
 -- FIRST
 select
     style as detail
 from
-    carcass
-left outer join car on (carcass.id = car.carcass_id)
-where car.carcass_id is not null
-union
+    car
+right outer join carcass on (car.carcass_id = carcass.id)
+where car.carcass_id is null
+union all
 
 -- SECOND
 select
     type as detail
 from
-    engine
-left outer join car on (engine.id = car.engine_id)
-where car.engine_id is not null
-union
+    car
+right outer join engine on (car.engine_id = engine.id)
+where car.engine_id is null
+union all
 
 -- THIRD
 select
     type as detail
 from
-    transmission
-left outer join car on (transmission.id = car.transmission_id)
-where car.transmission_id is not null;
+    car
+right outer join transmission on (transmission.id = car.transmission_id)
+where car.transmission_id is null;
 
