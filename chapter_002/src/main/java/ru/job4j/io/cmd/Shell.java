360bcd0ba12faf31c18292f399ab87e41d15500f
MerkurevSergei <MerkurevSergei@yandex.ru>
2020-06-14 13:53:05 +0900

    Мини cmd [#32826] - fix №1

--- chapter_002/src/main/java/ru/job4j/io/cmd/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/cmd/Shell.java
@@ -2,10 +2,28 @@ package ru.job4j.io.cmd;
 
 import java.util.*;
 
+/**
+ * The Shell is class emulate command shell.
+ *
+ * @author Merkurev Sergei (merkurevsergei@yandex.ru)
+ * @version 0.1
+ * @since 0.1
+ */
 class Shell {
-    private final LinkedList<String> path = new LinkedList<>();
-    private final Map<String, Runnable> operators = new HashMap<>();
 
+    /**
+     * Stored the elements of path.
+     */
+    private final LinkedList<String> path = new LinkedList<>();
+
+    /**
+     * Stored available operators.
+     */
+    private final Map<String, Command> operators = new HashMap<>();
+
+    /**
+     * Initialization operators.
+     */
     public Shell() {
         operators.put("/", path::clear);
         operators.put("", () -> { });
@@ -13,23 +31,40 @@ class Shell {
         operators.put("..", path::pollLast);
     }
 
+    /**
+     * @param path is destination path.
+     * @return this object with new state after execute command from destination path.
+     */
     public Shell cd(final String path) {
         if (path.startsWith("/")) {
-            operators.get("/").run();
+            operators.get("/").execute();
         }
         final String[] commands = path.trim().split("/");
         for (String command : commands) {
-            final Runnable operator = operators.getOrDefault(command, null);
+            final Command operator = operators.getOrDefault(command, null);
             if (operator == null) {
                 this.path.add(command);
             } else {
-                operator.run();
+                operator.execute();
             }
         }
         return this;
     }
 
+    /**
+     * @return current path.
+     */
     public String path() {
         return "/" + String.join("/", path);
+    }
+
+    /**
+     * The Command interface.
+     */
+    private interface Command {
+        /**
+         * Execute command logic.
+         */
+        void execute();
     }
 }
